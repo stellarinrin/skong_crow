fc305342c3f67249a4c95a63f00d8d3f02d3912c
stellarinrin <stellarinrin.922@gmail.com>
2026-01-25 13:41:02 -0600

    Add alternate sprites and direction facing

--- skong-crow/player/crow.gd
+++ skong-crow/player/crow.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@onready var animations: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+
 @export var speed: int = 240
 @export var movement_acceleration: int = 20
 @export var dash_speed: int = 1000
@@ -15,6 +18,8 @@ var dash_count: int = 0
 @export var max_midair_jumps: int = 1
 var midair_jump_count: int = 0
 
+var is_facing_left: bool = false
+
 func _physics_process(delta: float) -> void:
 	handle_input()
 	move_and_slide()
@@ -23,6 +28,7 @@ func _physics_process(delta: float) -> void:
 func handle_input() -> void:
 	#Horizontal Movement
 	var direction = Input.get_axis("ui_left","ui_right")
+	
 	if direction == 0:
 		velocity.x = move_toward(velocity.x, 0, movement_acceleration)
 	elif Input.is_action_just_pressed("dash") and dash_count < max_dash_count:
@@ -30,24 +36,56 @@ func handle_input() -> void:
 		dash_count = 1
 	else:
 		velocity.x = move_toward(velocity.x, speed * direction, movement_acceleration)
-	
+		
 	#Ground Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = ground_jump_speed
+		animations.play("glide")
 	
+	#Falling
+	if not is_on_floor() and not is_on_wall():
+		gravity = base_gravity
+		animations.play("air")
+	elif is_on_wall():
+		dash_count = 0
+		midair_jump_count = 0
+		animations.play("ground")
+	elif is_on_floor():
+		gravity = base_gravity
+		dash_count = 0
+		midair_jump_count = 0
+		animations.play("ground")
+
 	#Midair Jump
 	if Input.is_action_just_pressed("jump") and not is_on_floor() \
 			and midair_jump_count < max_midair_jumps:
 		velocity.y = midair_jump_speed
 		midair_jump_count += 1
-	if is_on_floor() or is_on_wall():
+		animations.play("glide")
+	if is_on_floor():
 		dash_count = 0
 		midair_jump_count = 0
+		animations.play("ground")
 	
 	#Float / Glide
-	if Input.is_action_pressed("jump") and velocity.y >= 0:
+	if Input.is_action_pressed("jump") and velocity.y > 0 \
+			and not is_on_floor() and not is_on_wall():
 		gravity = float_speed
-	else:
-		gravity = base_gravity
+		animations.play("glide")
+		print(velocity.y)
+
+	if (abs(velocity.x) - speed) > 0:
+		animations.play("glide")
+		
+	if direction > 0:
+		if not is_facing_left:
+			return
+		sprite.flip_h = false
+		is_facing_left = false
+	elif direction < 0:
+		if is_facing_left:
+			return
+		sprite.flip_h = true
+		is_facing_left = true
 	
 	
